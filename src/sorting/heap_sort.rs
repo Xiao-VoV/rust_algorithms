2619f9c4c4506835377ed385e9de0b200a4775e9
XP <a1679190@gmail.com>
2024-06-01 14:27:03 +0800

    堆排序修正

--- src/sorting/heap_sort.rs
+++ src/sorting/heap_sort.rs
@@ -1,38 +1,38 @@
 use std::{iter::Rev, mem::swap};
 
 // 堆排序下标从 1 开始如果从 0 开始则不满足左子树为 2i 的规律
-pub fn heap_sort<T: Ord>(vec: &mut [T]) {
+pub fn heap_sort_start_at_1<T: Ord>(vec: &mut [T]) {
     build_heap(vec);
     for i in (1..vec.len()).rev() {
         vec.swap(i, 1);
-        heapify(vec, 1);
+        heapify(vec, 1, i);
     }
 }
 
 fn build_heap<T: Ord>(vec: &mut [T]) {
-    let helf = vec.len() / 2;
+    let helf = (vec.len()) / 2;
     for i in (1..=helf).rev() {
-        heapify(vec, i);
+        heapify(vec, i, vec.len());
     }
 }
 
-fn heapify<T: Ord>(vec: &mut [T], i: usize) {
+fn heapify<T: Ord>(vec: &mut [T], i: usize, max_index: usize) {
     let left = i * 2;
     let right = left + 1;
-    let mut min;
-    if left < vec.len() && vec[left] < vec[i] {
-        min = left;
+    let mut max;
+    if left < max_index && vec[left] > vec[i] {
+        max = left;
     } else {
-        min = i
+        max = i
     }
 
-    if right < vec.len() && vec[right] < vec[i] {
-        min = right;
+    if right < max_index && vec[right] > vec[max] {
+        max = right;
     }
 
-    if min != i {
-        vec.swap(i, min);
-        heapify(vec, min)
+    if max != i {
+        vec.swap(i, max);
+        heapify(vec, max, max_index);
     }
 }
 
@@ -42,7 +42,7 @@ mod test {
     #[test]
     pub fn test_heap_sort() {
         let mut vec = vec![0, 1, 23, 4, 2, 77, 12, 888, 45, 6, 343, 33, 09];
-        heap_sort(&mut vec);
+        heap_sort_start_at_1(&mut vec);
         println!("{:?}", vec);
     }
 }
